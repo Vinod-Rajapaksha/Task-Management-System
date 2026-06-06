a8db0be1c268e1f02370df2c83ffcac1dd1da2dc
Malki Yasara <malkiyasara2004@gmail.com>
2026-06-06 10:07:32 +0530

    feat(auth): implement auth module

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,59 @@
+import { registerUser, loginUser } from "../services/auth.service.js";
+
+export const register = async ( req, res, next ) => {
+    try {
+      const {
+        name,
+        email,
+        password,
+      } = req.body;
+
+      const user =
+        await registerUser(
+          name,
+          email,
+          password
+        );
+
+      res.status(201).json({
+        success: true,
+        message:
+          "User Registered Successfully",
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+export const login = async ( req, res, next ) => {
+    try {
+      const { email, password } =
+        req.body;
+
+      const result =
+        await loginUser(
+          email,
+          password
+        );
+
+      res.status(200).json({
+        success: true,
+        ...result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+export const logout = async ( req, res ) => {
+    res.status(200).json({
+      success: true,
+      message:
+        "Logout Successful",
+    });
+  };
---
+++ server/src/services/auth.service.js
@@ -0,0 +1,78 @@
+import User from "../models/User.model.js";
+import { hashPassword, comparePassword } from "../utils/password.js";
+import { generateAccessToken, generateRefreshToken } from "../utils/jwt.js";
+
+export const registerUser =
+  async (
+    name,
+    email,
+    password
+  ) => {
+    const userExists =
+      await User.findOne({
+        email,
+      });
+
+    if (userExists) {
+      throw new Error(
+        "User already exists"
+      );
+    }
+
+    const hashedPassword =
+      await hashPassword(password);
+
+    const user =
+      await User.create({
+        name,
+        email,
+        password: hashedPassword,
+      });
+
+    return user;
+  };
+
+export const loginUser =
+  async (email, password) => {
+    const user =
+      await User.findOne({
+        email,
+      });
+
+    if (!user) {
+      throw new Error(
+        "Invalid Email or Password"
+      );
+    }
+
+    const isMatch =
+      await comparePassword(
+        password,
+        user.password
+      );
+
+    if (!isMatch) {
+      throw new Error(
+        "Invalid Email or Password"
+      );
+    }
+
+    return {
+      accessToken:
+        generateAccessToken(
+          user._id
+        ),
+
+      refreshToken:
+        generateRefreshToken(
+          user._id
+        ),
+
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    };
+  };
